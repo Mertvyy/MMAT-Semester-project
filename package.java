3b32e2534aa7d3ab0df845bfdc4acdd5a9029b52
Mertvyy <137713445+Mertvyy@users.noreply.github.com>
2026-04-21 14:41:20 +0300

    base package class

---
+++ package.java
@@ -0,0 +1,5 @@
+public class Package{
+    String id,destination;
+    public Package(String id,String dest){this.id=id;this.destination=dest;}
+    public String toString(){return id+" -> "+destination;}
+}
