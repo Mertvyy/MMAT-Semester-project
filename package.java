e439e6c9e7873627a0ad8d5b741ef7567796987e
Mertvyy <137713445+Mertvyy@users.noreply.github.com>
2026-04-28 17:19:33 +0300

    Add file reading functionality in Package class

--- package.java
+++ package.java
@@ -1,3 +1,6 @@
+InputStream is=getClass().getClassLoader().getResourceAsStream("packageData.txt");
+BufferedReader br=new BufferedReader(new InputStreamReader(is));
+
 public class Package{
     String id,destination;
     public Package(String id,String dest){this.id=id;this.destination=dest;}
