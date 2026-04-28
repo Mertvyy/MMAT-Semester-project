b92c70a1dfa1c1cf5688e462b653ff71855beb0d
Mertvyy <137713445+Mertvyy@users.noreply.github.com>
2026-04-28 17:11:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,117 +1,65 @@
 const API="https://mmat-semester-project.onrender.com";
 
-/* -------- DLL BUFFER -------- */
-async function addToBuffer(){
-let id=document.getElementById("pkgId").value;
-let dest=document.getElementById("dest").value;
 
-await fetch(API_URL+"/buffer/add",{
+/* -------- SHIPMENTS -------- */
+
+async function addShipment(){
+let id=document.getElementById("shipId").value;
+let addr=document.getElementById("address").value;
+
+await fetch(API+"/shipment/add",{
 method:"POST",
 headers:{"Content-Type":"application/json"},
-body:JSON.stringify({id,dest})
+body:JSON.stringify({id,address:addr})
 });
 
-loadBuffer();
+printAll();
 }
 
-async function removeFromBuffer(){
-await fetch(API_URL+"/buffer/remove",{method:"POST"});
-loadBuffer();
+async function deleteShipment(){
+let id=document.getElementById("shipId").value;
+
+await fetch(API+"/shipment/delete",{
+method:"POST",
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({id})
+});
+
+printAll();
 }
 
-async function loadBuffer(){
-let res=await fetch(API_URL+"/buffer");
+async function printAll(){
+let res=await fetch(API+"/shipment/all");
 let data=await res.json();
 
-let div=document.getElementById("buffer");
-div.innerHTML="";
+let box=document.getElementById("shipments");
+box.innerHTML="";
 
-data.forEach(p=>{
-div.innerHTML+=`<div class="item"><span>${p.id}</span><span>${p.destination}</span></div>`;
+data.forEach(s=>{
+box.innerHTML+=`<div class="item">
+<span>${s.id}</span>
+<span>${s.address}</span>
+</div>`;
 });
 }
 
-/* -------- QUEUE -------- */
-async function enqueue(){
-await fetch(API_URL+"/queue/enqueue",{method:"POST"});
-loadQueue();
+/* -------- ADDRESS SEARCH (AVL) -------- */
+
+async function searchAddress(){
+let q=document.getElementById("search").value;
+
+let res=await fetch(API+"/address/search?key="+q);
+document.getElementById("searchOut").innerText=await res.text();
 }
 
-async function dequeue(){
-await fetch(API_URL+"/queue/dequeue",{method:"POST"});
-loadQueue();
+/* -------- OPTIMAL ROUTE (DIJKSTRA / KRUSKAL) -------- */
+
+async function buildRoute(){
+let res=await fetch(API+"/route/optimal");
+let data=await res.text();
+
+document.getElementById("routeOut").innerText=data;
 }
 
-async function loadQueue(){
-let res=await fetch(API_URL+"/queue");
-let data=await res.json();
+printAll();
 
-let div=document.getElementById("queue");
-div.innerHTML="";
-data.forEach(p=>{
-div.innerHTML+=`<div class="item">${p.id}</div>`;
-});
-}
-
-/* -------- STACK -------- */
-async function pushTruck(){
-await fetch(API_URL+"/stack/push",{method:"POST"});
-loadStack();
-}
-
-async function popTruck(){
-await fetch(API_URL+"/stack/pop",{method:"POST"});
-loadStack();
-}
-
-async function loadStack(){
-let res=await fetch(API_URL+"/stack");
-let data=await res.json();
-
-let div=document.getElementById("stack");
-div.innerHTML="";
-data.forEach(p=>{
-div.innerHTML+=`<div class="item">${p.id}</div>`;
-});
-}
-
-/* -------- GRAPH -------- */
-async function shortestPath(){
-let s=document.getElementById("start").value;
-let e=document.getElementById("end").value;
-
-let res=await fetch(`${API_URL}/graph/shortest?start=${s}&end=${e}`);
-document.getElementById("graphOut").innerText=await res.text();
-}
-
-async function mst(){
-let res=await fetch(API_URL+"/graph/mst");
-document.getElementById("graphOut").innerText=await res.text();
-}
-
-/* -------- AVL -------- */
-async function searchAVL(){
-let key=document.getElementById("searchKey").value;
-
-let res=await fetch(`${API_URL}/avl/search?key=${key}`);
-document.getElementById("avlOut").innerText=await res.text();
-}
-
-/* -------- SLL LOG -------- */
-async function loadLog(){
-let res=await fetch(API_URL+"/log");
-let data=await res.json();
-
-let div=document.getElementById("log");
-div.innerHTML="";
-
-data.forEach(p=>{
-div.innerHTML+=`<div class="item">${p.id} -> ${p.destination}</div>`;
-});
-}
-
-/* init */
-loadBuffer();
-loadQueue();
-loadStack();
-loadLog();
