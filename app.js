72ad6e918322169ba1d10a22674696315ab314e4
Mertvyy <137713445+Mertvyy@users.noreply.github.com>
2026-04-28 16:46:36 +0300

    Refactor API calls to use API_URL

--- app.js
+++ app.js
@@ -1,53 +1,117 @@
 const API="https://mmat-semester-project.onrender.com";
 
-async function load(){
-let res=await fetch(API+"/packages");
-let data=await res.json();
-
-let list=document.getElementById("list");
-list.innerHTML="";
-
-data.forEach(p=>{
-let div=document.createElement("div");
-div.innerHTML=`<span>${p.id}</span><span>${p.destination}</span>`;
-list.appendChild(div);
-});
-}
-
-async function enqueue(){
+/* -------- DLL BUFFER -------- */
+async function addToBuffer(){
+let id=document.getElementById("pkgId").value;
 let dest=document.getElementById("dest").value;
 
-await fetch(API+"/enqueue",{
+await fetch(API_URL+"/buffer/add",{
 method:"POST",
 headers:{"Content-Type":"application/json"},
-body:JSON.stringify({destination:dest})
+body:JSON.stringify({id,dest})
 });
 
-load();
+loadBuffer();
 }
 
-async function process(){
-await fetch(API+"/process",{method:"POST"});
-load();
+async function removeFromBuffer(){
+await fetch(API_URL+"/buffer/remove",{method:"POST"});
+loadBuffer();
 }
 
-async function route(){
+async function loadBuffer(){
+let res=await fetch(API_URL+"/buffer");
+let data=await res.json();
+
+let div=document.getElementById("buffer");
+div.innerHTML="";
+
+data.forEach(p=>{
+div.innerHTML+=`<div class="item"><span>${p.id}</span><span>${p.destination}</span></div>`;
+});
+}
+
+/* -------- QUEUE -------- */
+async function enqueue(){
+await fetch(API_URL+"/queue/enqueue",{method:"POST"});
+loadQueue();
+}
+
+async function dequeue(){
+await fetch(API_URL+"/queue/dequeue",{method:"POST"});
+loadQueue();
+}
+
+async function loadQueue(){
+let res=await fetch(API_URL+"/queue");
+let data=await res.json();
+
+let div=document.getElementById("queue");
+div.innerHTML="";
+data.forEach(p=>{
+div.innerHTML+=`<div class="item">${p.id}</div>`;
+});
+}
+
+/* -------- STACK -------- */
+async function pushTruck(){
+await fetch(API_URL+"/stack/push",{method:"POST"});
+loadStack();
+}
+
+async function popTruck(){
+await fetch(API_URL+"/stack/pop",{method:"POST"});
+loadStack();
+}
+
+async function loadStack(){
+let res=await fetch(API_URL+"/stack");
+let data=await res.json();
+
+let div=document.getElementById("stack");
+div.innerHTML="";
+data.forEach(p=>{
+div.innerHTML+=`<div class="item">${p.id}</div>`;
+});
+}
+
+/* -------- GRAPH -------- */
+async function shortestPath(){
 let s=document.getElementById("start").value;
 let e=document.getElementById("end").value;
 
-let res=await fetch(`${API}/shortest-path?start=${s}&end=${e}`);
-let data=await res.text();
-
-document.getElementById("routeResult").innerText=data;
+let res=await fetch(`${API_URL}/graph/shortest?start=${s}&end=${e}`);
+document.getElementById("graphOut").innerText=await res.text();
 }
 
-async function search(){
-let k=document.getElementById("searchKey").value;
-
-let res=await fetch(`${API}/search?key=${k}`);
-let data=await res.text();
-
-document.getElementById("searchResult").innerText=data;
+async function mst(){
+let res=await fetch(API_URL+"/graph/mst");
+document.getElementById("graphOut").innerText=await res.text();
 }
 
-load();
+/* -------- AVL -------- */
+async function searchAVL(){
+let key=document.getElementById("searchKey").value;
+
+let res=await fetch(`${API_URL}/avl/search?key=${key}`);
+document.getElementById("avlOut").innerText=await res.text();
+}
+
+/* -------- SLL LOG -------- */
+async function loadLog(){
+let res=await fetch(API_URL+"/log");
+let data=await res.json();
+
+let div=document.getElementById("log");
+div.innerHTML="";
+
+data.forEach(p=>{
+div.innerHTML+=`<div class="item">${p.id} -> ${p.destination}</div>`;
+});
+}
+
+/* init */
+loadBuffer();
+loadQueue();
+loadStack();
+loadLog();
