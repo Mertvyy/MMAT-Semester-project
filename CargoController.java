53d2440af5a888eac422d346da4e832eb3a4dfa5
Mertvyy <137713445+Mertvyy@users.noreply.github.com>
2026-05-12 14:18:53 +0300

    Update CargoController.java

--- CargoController.java
+++ CargoController.java
@@ -6,92 +6,82 @@ import java.io.*;
 @CrossOrigin("*")
 public class CargoController {
 
-    DLL buffer=new DLL();
-    SLL log=new SLL();
-    AVLTree avl=new AVLTree();
-    Graph graph=new Graph();
+    DLL buffer = new DLL();
+    SLL log = new SLL();
+    AVLTree avl = new AVLTree();
+    Graph graph = new Graph();
 
     public CargoController() throws Exception {
 
-        List<Package> pkgs=
-        FileManager.readPackages("packageData.txt");
+        List<Package> pkgs =
+                FileManager.readPackages("packageData.txt");
 
-        for(Package p:pkgs){
+        for (Package p : pkgs) {
             buffer.insertAtTail(p);
             log.addRecord(p);
-            avl.insert(p.destination,p.id);
+            avl.insert(p.destination, p.id);
         }
 
-        graph=FileManager.readMap("mapData.txt");
+        graph = FileManager.readMap("mapData.txt");
     }
 
     @GetMapping("/shipment/all")
-    public List<Package> all(){
+    public List<Package> all() {
 
-        List<Package> out=new ArrayList<>();
+        List<Package> out = new ArrayList<>();
+        DLLNode t = buffer.head;
 
-        DLLNode t=buffer.head;
-
-        while(t!=null){
+        while (t != null) {
             out.add(t.data);
-            t=t.next;
+            t = t.next;
         }
 
         return out;
     }
 
     @PostMapping("/shipment/add")
-    public void add(@RequestBody Package p)throws Exception{
+    public void add(@RequestBody Package p) throws Exception {
 
         buffer.insertAtTail(p);
         log.addRecord(p);
-        avl.insert(p.destination,p.id);
+        avl.insert(p.destination, p.id);
 
-        FileWriter fw=
-        new FileWriter("packageData.txt",true);
-
-        fw.write("\n"+p.id+" "+p.destination);
-
+        FileWriter fw = new FileWriter("packageData.txt", true);
+        fw.write("\n" + p.id + " " + p.destination);
         fw.close();
     }
 
     @PostMapping("/shipment/delete")
-    public void delete(@RequestBody Map<String,String> body){
+    public void delete(@RequestBody Map<String, String> body) {
 
-        String id=body.get("id");
+        String id = body.get("id");
 
-        Package removed=null;
+        DLLNode t = buffer.head;
 
-        DLLNode t=buffer.head;
+        while (t != null) {
 
-        while(t!=null){
-
-            if(t.data.id.equals(id)){
-                removed=t.data;
+            if (t.data.id.equals(id)) {
+                buffer.removePackage(t.data);
                 break;
             }
 
-            t=t.next;
+            t = t.next;
         }
-
-        if(removed!=null)
-            buffer.removePackage(removed);
     }
 
     @GetMapping("/address/search")
-    public String search(@RequestParam String key){
+    public String search(@RequestParam String key) {
 
-        String res=avl.search(key);
+        String res = avl.search(key);
 
-        if(res==null)
+        if (res == null)
             return "Address not found";
 
-        return "Shipment ID: "+res;
+        return "Shipment ID: " + res;
     }
 
     @GetMapping("/route/optimal")
-    public String route(){
-
+    public String route() {
         return graph.getMSTText();
     }
 }
