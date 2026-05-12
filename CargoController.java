59d3293fd05740d0cb1dbd2023a81d6a70983d04
Mertvyy <137713445+Mertvyy@users.noreply.github.com>
2026-05-12 13:46:12 +0300

    Update CargoController.java

--- CargoController.java
+++ CargoController.java
@@ -1,24 +1,58 @@
-import java.util.*;
 import org.springframework.web.bind.annotation.*;
+import java.util.*;
+import java.io.*;
 
 @RestController
 @CrossOrigin("*")
-public class CargoController{
+public class CargoController {
 
-    private final DataStore ds;
+    DLL buffer=new DLL();
+    SLL log=new SLL();
+    AVLTree avl=new AVLTree();
+    Graph graph=new Graph();
 
-    public CargoController(DataStore ds){
-        this.ds=ds;
+    public CargoController() throws Exception {
+
+        List<Package> pkgs=
+        FileManager.readPackages("packageData.txt");
+
+        for(Package p:pkgs){
+            buffer.insertAtTail(p);
+            log.addRecord(p);
+            avl.insert(p.destination,p.id);
+        }
+
+        graph=FileManager.readMap("mapData.txt");
     }
 
     @GetMapping("/shipment/all")
-    public List<PackageItem> all(){
-        return ds.packages;
+    public List<Package> all(){
+
+        List<Package> out=new ArrayList<>();
+
+        DLLNode t=buffer.head;
+
+        while(t!=null){
+            out.add(t.data);
+            t=t.next;
+        }
+
+        return out;
     }
 
     @PostMapping("/shipment/add")
-    public void add(@RequestBody PackageItem p){
-        ds.packages.add(p);
+    public void add(@RequestBody Package p)throws Exception{
+
+        buffer.insertAtTail(p);
+        log.addRecord(p);
+        avl.insert(p.destination,p.id);
+
+        FileWriter fw=
+        new FileWriter("packageData.txt",true);
+
+        fw.write("\n"+p.id+" "+p.destination);
+
+        fw.close();
     }
 
     @PostMapping("/shipment/delete")
@@ -26,6 +60,38 @@ public class CargoController{
 
         String id=body.get("id");
 
-        ds.packages.removeIf(p->p.id.equals(id));
+        Package removed=null;
+
+        DLLNode t=buffer.head;
+
+        while(t!=null){
+
+            if(t.data.id.equals(id)){
+                removed=t.data;
+                break;
+            }
+
+            t=t.next;
+        }
+
+        if(removed!=null)
+            buffer.removePackage(removed);
+    }
+
+    @GetMapping("/address/search")
+    public String search(@RequestParam String key){
+
+        String res=avl.search(key);
+
+        if(res==null)
+            return "Address not found";
+
+        return "Shipment ID: "+res;
+    }
+
+    @GetMapping("/route/optimal")
+    public String route(){
+
+        return graph.getMSTText();
     }
 }
