aec57c59fb165a89b7b86c61a5b5f2d58349e575
Mertvyy <137713445+Mertvyy@users.noreply.github.com>
2026-05-12 13:32:24 +0300

    Create PackageItem class with constructors

---
+++ PackageItem.java
@@ -0,0 +1,11 @@
+public class PackageItem{
+    public String id;
+    public String destination;
+
+    public PackageItem(){}
+
+    public PackageItem(String id,String destination){
+        this.id=id;
+        this.destination=destination;
+    }
+}
