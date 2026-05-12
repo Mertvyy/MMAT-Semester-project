70b80c801a0c6a18dedd757304a2d678c169e5f9
Mertvyy <137713445+Mertvyy@users.noreply.github.com>
2026-05-12 14:21:13 +0300

    Update Package.java

--- Package.java
+++ Package.java
@@ -3,24 +3,29 @@
  * Represents a single delivery item in the logistics system.
  * Acts as a simple data model (POJO) to hold the package ID and its destination.
  */
-public class Package {
     
-    String id;          // Unique identifier (e.g., PKG_KYS_001)
-    String destination; // Target neighborhood (e.g., Talas, Belsin)
-
-    /**
+       /* Unique identifier (e.g., PKG_KYS_001)
+     Target neighborhood (e.g., Talas, Belsin)
      * Constructor to initialize a new package.
      * * @param id The unique package ID.
      * @param dest The destination neighborhood.
      */
 
 
-public Package(){}
-    
-    public Package(String id, String dest) {
+
+
+    public class Package {
+
+    public String id;
+    public String destination;
+
+    public Package() {}
+
+    public Package(String id, String destination) {
         this.id = id;
-        this.destination = dest;
+        this.destination = destination;
     }
+}
 
     /**
      * Returns a string representation of the package for easy console printing.
